f03d6b8553d10c248622cfafecf5f308047a0802
Jack <devjack050700@gmail.com>
2026-09-25 15:45:18 +0900

    chore: 스프링 프로젝트 초기 설정

---
+++ src/main/java/com/muckzi/MuckziApplication.java
@@ -0,0 +1,13 @@
+package com.muckzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuckziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuckziApplication.class, args);
+	}
+
+}
